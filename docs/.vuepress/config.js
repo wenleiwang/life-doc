4a85e8552429935d4c4d42a6aadb604885a20340
wangwenlei <wangwenlei@fang.com>
2022-01-19 17:36:11 +0800

    添加git

---
+++ docs/.vuepress/config.js
@@ -0,0 +1,35 @@
+module.exports = {
+    title: '文文的文档',
+    description: '只为了记录',
+    head:[
+      ['link',{rel:'icon',href:'/favicon.ico'}]
+    ],
+    themeConfig: {
+      logo: '/logo.png',
+      nav: [
+        { text: '首页', link: '/' },
+        { text: 'Java', link: '/java/' },
+        { text: 'MySQL', link: '/mysql/' },
+        { text: 'Redis', link: '/redis/' },
+        { text: 'RabbitMQ', link: '/rabbitmq/', target:'_blank' },
+      ],
+      sidebar: {
+        '/java/': [
+          '',
+          'java',
+          'spring',
+        ],
+        '/mysql/': [
+          '',
+        ],
+        '/redis/': [
+          '',
+        ],
+        '/rabbitmq/': [
+          '',
+        ]
+      },
+      displayAllHeaders: true, // 显示所有页面的标题链接
+      activeHeaderLinks: true,// 显示活动的标题链接
+    }
+}
---
+++ docs/java/readme.md
@@ -0,0 +1,12 @@
+---
+---
+# java1
+人事
+
+## dalsdjflasdjf
+人事
+
+### dafsodfjo
+人事
+# java2
+人事
